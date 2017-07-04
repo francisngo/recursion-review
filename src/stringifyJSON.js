44027882627d0a6b168fba0629142b1fdb68a387
francisngo <francis.t.ngo@gmail.com>
2017-07-04 14:12:53 -0700

    add code to stringify string, number, boolean, function, undefined, null and array

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,53 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+  if (typeof obj === 'boolean') {
+    return obj.toString();
+  }
+  if (typeof obj === 'function') {
+    return undefined;
+  }
+  if (typeof obj === undefined) {
+    return undefined;
+  }
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (Array.isArray(obj)) {
+    var i = 0;
+    while (i < obj.length) {
+      obj[i] = stringifyJSON(obj[i]);
+      i++;
+    }
+    return '[' + obj + ']';
+  }
+
+  if (typeof obj === 'object') {
+    var keys = Object.keys(obj);
+    var str = '';
+    var i = 0;
+    while (i < keys.length) {
+      if (key === 'functions' || key === undefined) {
+        return '{}';
+      } else {
+        if (index === keys.length - 1) {
+          var key = keys[i];
+          str += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+        } else {
+          str += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+        }
+      }
+      index++;
+    }
+    return '{' + str + '}';
+  }
 };
